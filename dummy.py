a1aa45c9cc752a80602b950f5d3fab0c96286bb0
prerana2005 <145919951+prerana2005@users.noreply.github.com>
2025-06-25 15:43:09 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,1 +1,14 @@
-def add(a, b): return a + b
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def complex_function(x):
+    if x > 0:
+        for i in range(x):
+            if i % 2 == 0:
+                print(factorial(i))
+            else:
+                for j in range(i):
+                    print(j)
