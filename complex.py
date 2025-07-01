99606381ba051eb18ba588213aaacaae6a658f49
prerana2005 <preranamp2005@gmail.com>
2025-07-01 10:45:40 +0530

    Add complex function

---
+++ complex.py
@@ -0,0 +1,9 @@
+def complex_function(x):
+    if x > 0:
+        for i in range(10):
+            if i % 2 == 0:
+                print(i)
+            else:
+                for j in range(5):
+                    if j > 2:
+                        print(j)
