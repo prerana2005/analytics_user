f4887b758e69d54687dcf326684b84b4033e92e1
prerana2005 <preranamp2005@gmail.com>
2025-07-01 11:18:00 +0530

    complex function

--- complex.py
+++ complex.py
@@ -1,9 +1,23 @@
 def complex_function(x):
     if x > 0:
-        for i in range(10):
-            if i % 2 == 0:
-                print(i)
-            else:
-                for j in range(5):
-                    if j > 2:
-                        print(j)
+        print("Positive")
+    if x < 0:
+        print("Negative")
+    if x == 0:
+        print("Zero")
+    if x % 2 == 0:
+        print("Even")
+    if x % 3 == 0:
+        print("Divisible by 3")
+    if x % 5 == 0:
+        print("Divisible by 5")
+    if x % 7 == 0:
+        print("Divisible by 7")
+    if x % 11 == 0:
+        print("Divisible by 11")
+    if x % 13 == 0:
+        print("Divisible by 13")
+    if x % 17 == 0:
+        print("Divisible by 17")
+    if x % 19 == 0:
+        print("Divisible by 19")
